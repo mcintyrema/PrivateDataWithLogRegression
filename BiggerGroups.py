6d6f6e241a8a15906a1f361fec2f2b72cb255bc0
root <root@kenzieslaptop>
2024-07-25 13:11:37 -0400

    train k groups

--- BiggerGroups.py
+++ BiggerGroups.py
@@ -33,8 +33,7 @@ age_hierarchy_builder = IntervalHierarchyBuilder()
 age_hierarchy_builder.add_interval(-1, 0, "unknown")
 age_hierarchy_builder.add_interval(0, 18, "child")
 age_hierarchy_builder.add_interval(18, 30, "young-adult")
-age_hierarchy_builder.add_interval(30, 60, "adult")
-age_hierarchy_builder.add_interval(60, 75, "older adult")
+age_hierarchy_builder.add_interval(30, 75, "adult")
 age_hierarchy_builder.add_interval(75, 110, "elderly")
 age_hierarchy = arxaas.hierarchy(age_hierarchy_builder, df['age'].tolist())
 
@@ -43,8 +42,7 @@ education_hierarchy_builder = IntervalHierarchyBuilder()
 education_hierarchy_builder.add_interval(-1, 0, "unknown")
 education_hierarchy_builder.add_interval(0, 9, "Some school")
 education_hierarchy_builder.add_interval(9, 10, "HS-grad")
-education_hierarchy_builder.add_interval(10, 14, "Undergrad")
-education_hierarchy_builder.add_interval(14, 23, "Grad School")
+education_hierarchy_builder.add_interval(10, 23, "Higher Educatoin")
 education_hierarchy = arxaas.hierarchy(education_hierarchy_builder, df['education.num'].tolist())
 
 #add hierarchies
@@ -52,7 +50,7 @@ dataset.set_hierarchy('age', age_hierarchy)
 dataset.set_hierarchy('education.num', education_hierarchy)
 
 #create privacy model
-kanon = KAnonymity(4)
+kanon = KAnonymity(2)
 
 anon_result = arxaas.anonymize(dataset, [kanon])
 #verify anonymization
---
+++ LogisticRegression.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+
+df = pd.read_csv("../adult.csv")
+
+# sampling
+sample_df = df.sample(frac=0.1, random_state=42)
+
+income_mapping = {
+    ">50K": 1,
+    "<=50K": 0,
+    '*': -1
+}
+df['income'].str.strip()
+sample_df['income'] = df['income'].map(income_mapping)
+
+X = sample_df.drop(columns=['income'])
+y = sample_df['income']  
+
+# convert categorical to numeric
+X = pd.get_dummies(X)
+
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# train model
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+# get predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
